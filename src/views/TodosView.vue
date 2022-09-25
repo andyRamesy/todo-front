9de3bcaa9f63062e6b86e25bb9f84290b20142af
andyRamesy <ramesy.richy@yahoo.com>
2022-09-25 22:22:01 +0300

    first try composition api

---
+++ src/views/TodosView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{test.name}}</h1>
+  </div>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+export default {
+    setup(){
+        const test = reactive({
+            name : "My name is Andy"
+        })
+        return {test}
+    }
+}
+</script>
+
+<style>
+
+</style>
