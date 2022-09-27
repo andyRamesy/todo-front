340ed22de6007fb78a869d5a5eefbd5278eb113f
andyRamesy <ramesy.richy@yahoo.com>
2022-09-27 07:59:50 +0300

    finish connecting back to front

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,21 +1,33 @@
 <template>
-  <div>
-    <h1>{{test.name}}</h1>
-  </div>
+	<div>
+		<div v-for="item in state.todos" :key="item.author">
+			<h4>
+				{{ item.author }}
+			</h4>
+			<p>
+				{{ item.todo }}
+			</p>
+			<button @click="deleteTodo(item._id)">Delete</button>
+			<button @click="editTodo(item._id)">Edit</button>
+		</div>
+		<button @click="newTodo">New</button>
+	</div>
 </template>
 
 <script>
-import { reactive } from '@vue/reactivity'
+import { onMounted } from "vue";
+import todocrud from "../modules/todocrud.js";
+
 export default {
-    setup(){
-        const test = reactive({
-            name : "My name is Andy"
-        })
-        return {test}
-    }
-}
+	setup() {
+		const { state, GetAllTodos, newTodo, deleteTodo, editTodo } = todocrud();
+
+		onMounted(() => {
+			GetAllTodos();
+		});
+		return { state, GetAllTodos, newTodo, deleteTodo, editTodo };
+	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
