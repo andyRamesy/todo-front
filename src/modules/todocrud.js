dbd4a64c8f5e52edcccac8b520ff4e0c334d0b4a
andyRamesy <ramesy.richy@yahoo.com>
2022-09-28 11:35:27 +0300

    finish edit todo

--- src/modules/todocrud.js
+++ src/modules/todocrud.js
@@ -1,12 +1,12 @@
 import { ref, computed } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const getTodo = () => {
 	const route = useRoute();
-	// const router = useRouter()
+	const router = useRouter()
 
 	const todoId = computed(() => route.params.id);
-	console.log("todoId:", todoId);
+	console.log("todoId:", todoId.value);
 
 	const state = ref({
 		newAuthor: "",
@@ -32,7 +32,7 @@ const getTodo = () => {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json"
-				
+
 				// "auth-token": state.token
 			},
 			body: JSON.stringify({
@@ -45,21 +45,47 @@ const getTodo = () => {
 		);
 	};
 
-	const deleteTodo = (_id) => {
-		fetch("http://localhost:3000/todos/delete/" + _id, {
+	const deleteTodo = () => {
+		fetch("http://localhost:3000/todos/delete/" + todoId.value, {
 			method: "DELETE"
 		});
 	};
-	const editTodo = (_id) => {
+	const editTodo = () => {
 		const requestOptions = {
-			method: "PUT"
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+				// "auth-token": state.token
+			},
+			body: JSON.stringify({
+				author: state.value.newAuthor,
+				todo: state.value.newTodoItem
+			})
 		};
-		fetch("http://localhost:3000/todos/update/" + _id, requestOptions)
+		fetch("http://localhost:3000/todos/update/" + todoId.value, requestOptions)
 			.then((res) => res.body)
 			.then((res) => console.log(res));
+			router.push("/todos");
+	};
+
+	//get the clicked id
+	const todo = ref({});
+	const GetSpecificTodo = async () => {
+		try {
+			fetch("http://localhost:3000/todos")
+				.then((res) => res.json())
+				.then((data) => {
+					todo.value = data.filter((todo) => todo._id === todoId.value);
+				});
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return {
+		todoId,
+		todo,
+		GetSpecificTodo,
 		state,
 		GetAllTodos,
 		newTodo,
--- src/views/TodoDetail.vue
+++ src/views/TodoDetail.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
 		<h1>Edit a todo</h1>
-        <div v-for="item in todo" :key="item._id">
-            <h4>{{item.author}}</h4>
-            <h4>{{item.todo}}</h4>
-        </div>
+		<div v-for="item in todo" :key="item._id">
+			<h4>{{ item.author }}</h4>
+			<h4>{{ item.todo }}</h4>
+		</div>
 		<hr />
 		<input type="text" placeholder="Author" v-model="state.newAuthor" />
 		<span>Test: {{ state.newAuthor }}</span>
@@ -20,8 +20,11 @@
 import todocrud from "../modules/todocrud.js";
 export default {
 	setup() {
-		const { editTodo, state } = todocrud();
-		return { editTodo, state };
+		const { editTodo, state, GetSpecificTodo, todo, todoId } = todocrud();
+
+		GetSpecificTodo();
+
+		return { editTodo, state, todo, todoId };
 	}
 };
 </script>
